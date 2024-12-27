56b9c01323e7ae5136964039acf022e8b4f9d9af
elsayedgomaa <elsayed17jh2@gmail.com>
2024-12-27 21:05:34 +0200

    Update

--- src/LoanForm.js
+++ src/LoanForm.js
@@ -2,8 +2,11 @@ import './styleForm.css'
 import Modal from './Modal'
 import { useState } from 'react'
 
+
 export default function LoanForm() {
-    const [loaneInput, setloaneinput] =useState({
+    const [errorMsg, seterrormsg] = useState(null)
+    const [showModal, setshowmodal] = useState(false)
+    const [loaneInput, setloaneinput] = useState({
         name: "",
         phone: "",
         age: "",
@@ -26,38 +29,60 @@ export default function LoanForm() {
             });
         }
     }
+
+    // moadel
+    function handlsubmitMoadel(event) {
+        event.preventDefault();
+        
+        if (loaneInput.age < 18 || loaneInput.age > 100) {
+            seterrormsg("The age is not allowed")
+        } else if (loaneInput.phone.length !== 12) {
+            seterrormsg("Phone number must be exactly 10 digits")
+        } else {
+            seterrormsg(null)
+        
+        }
+        setshowmodal(true)
+    }
+ 
+// hide moadal 
+function hidemodal() {
+    setshowmodal(false)
+}
+
+// check btn disabled style  true or false
+let disabld = !loaneInput.name || !loaneInput.phone || !loaneInput.age;
+
+return (
+    <div className='flex' style={{ flexDirection: 'column' }} onClick={hidemodal}>
+        <form className='Loaenform' onSubmit={handlsubmitMoadel}>
+            <h1>Requesting a Loan </h1>
+            <hr></hr>
+            <label>Name</label>
+            <input name='name' value={loaneInput.name} onChange={handelValue}></input>
+
+            <label>Phone Number</label>
+            <input name='phone' value={loaneInput.phone} onChange={handelValue}></input>
+
+            <label>Age</label>
+            <input type='number' name='age' value={loaneInput.age} onChange={handelValue}></input>
+
+            <label style={{ marginTop: "30px" }}>Are you employee?</label>
+            <input name='isemployee' type='checkbox' checked={loaneInput.isemployee} onChange={handelValue}></input>
+
+            <label>Salary</label>
+            <select name='salary' onChange={handelValue}>
+                <option value="less">Less than 500$</option>
+                <option value="500-2000">Between 500$ and 2000</option>
+                <option value="above">Above 2000</option>
+            </select>
+
+            <button className={disabld ? 'checkdisbled' : ''} disabled={disabld} id='submitloaenBtn'>submit</button>
+
+        </form>
+        <Modal erroeMSg={errorMsg} ModalHidden={showModal} />
+    </div>
+
+)
     
-
-    // check btn disabled style  true or false
-    let disabld = !loaneInput.name || !loaneInput.phone || !loaneInput.age;
-
-    return (
-        <div className='flex' style={{ flexDirection: 'column' }}>
-            <form className='Loaenform'>
-                <h1>Requesting a Loan </h1>
-                <hr></hr>
-                <label>Name</label>
-                <input name='name' value={loaneInput.name} onChange={handelValue}></input>
-
-                <label>Phone Number</label>
-                <input name='phone' value={loaneInput.phone} onChange={handelValue}></input>
-
-                <label>Age</label>
-                <input name='age' value={loaneInput.age} onChange={handelValue}></input>
-
-                <label style={{ marginTop: "30px" }}>Are you employee?</label>
-                <input name='isemployee' type='checkbox' checked={loaneInput.isemployee} onChange={handelValue}></input>
-
-                <label>Salary</label>
-                <select name='salary' onChange={handelValue}>
-                    <option value="less">Less than 500$</option>
-                    <option value="500-2000">Between 500$ and 2000</option>
-                    <option value="above">Above 2000</option>
-                </select>
-
-                <button className={disabld ? 'checkdisbled' : ''} disabled={disabld} id='submitloaenBtn'>submit</button>
-
-            </form>
-        </div> 
-    )
 }
--- src/Modal.js
+++ src/Modal.js
@@ -1,9 +1,15 @@
 import './styleForm.css'
-export default function Modal() {
+export default function Modal({ModalHidden,erroeMSg}){
+  if(ModalHidden){
     return <div  id="modal">
-        <div id="modalcontent">
-          <h1 >The Form has been submited successfully.</h1>
-        </div>
-
+    <div id="modalcontent">
+ 
+ <h1  style={{color:erroeMSg?"red" :"green"}}>
+  {erroeMSg!=null?erroeMSg:"The Form has been submited successfully"}</h1>
     </div>
+</div>
+  }else{
+    <></>
+  }
+ 
 }
